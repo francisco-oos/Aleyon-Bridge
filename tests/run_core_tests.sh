8952cfbf6bc98e4b1145bf7ddef23aeb562dc67e
francisco-oos <alvaradoleyvaj@gmail.com>
2026-09-18 20:27:58 -0600

    test: include adaptive chat routing policy in core compile

--- tests/run_core_tests.sh
+++ tests/run_core_tests.sh
@@ -10,6 +10,7 @@ javac -d "$OUT" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/RecoveryPlanner.java" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/ProfileNaming.java" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/CanonicalConversationPolicy.java" \
+  "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/CanonicalChatRoutingPolicy.java" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/TransportState.java" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/ScreenBoundsPolicy.java" \
   "$ROOT/app/src/main/java/com/aleyon/geminibridge/core/LearningEvent.java" \
