6f3d310ed9b718cde3e61efcde0d7c33d4e3a7b1
francisco-oos <alvaradoleyvaj@gmail.com>
2026-09-18 20:45:48 -0600

    test: remove superseded planner from core runners

--- tests/run_core_tests.bat
+++ tests/run_core_tests.bat
@@ -13,7 +13,6 @@ javac -d "%OUT%" ^
  app\src\main\java\com\aleyon\geminibridge\core\ProfileNaming.java ^
  app\src\main\java\com\aleyon\geminibridge\core\CanonicalConversationPolicy.java ^
  app\src\main\java\com\aleyon\geminibridge\core\CanonicalChatRoutingPolicy.java ^
- app\src\main\java\com\aleyon\geminibridge\core\AdaptiveNavigationPlanner.java ^
  app\src\main\java\com\aleyon\geminibridge\core\TransportState.java ^
  app\src\main\java\com\aleyon\geminibridge\core\ScreenBoundsPolicy.java ^
  app\src\main\java\com\aleyon\geminibridge\core\LearningEvent.java ^
